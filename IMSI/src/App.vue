c21fcab397b0ac9a8487f2bd120d631404bb8248
nayoung <noela0202@naver.com>
2025-09-09 15:22:43 +0900

    25.09.09

--- IMSI/src/App.vue
+++ IMSI/src/App.vue
@@ -1,18 +1,7 @@
 <script setup>
-import ButtonCounter from '@/components/ButtonCounter.vue';
 
-const post = {
-  id : 1,
-  title : '이것은 제목입니다.',
-  mainCont : '이것은 본문입니다.',
-}
 </script>
 
 <template>
-<h1>아래 있는 컴포넌트는 자식 컴포넌트입니다.</h1>
-<ButtonCounter :title="post.title" :mainCont = "post.mainCont"/>
+
 </template>
-
-<style>
-
-</style>
---
+++ IMSI/src/components/ChildComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineEmits } from 'vue';
+
+const emit = defineEmits(['customEvent'])
+
+const handleClick = () => {
+emit('customEvent','Button clicked!')
+}
+</script>
+
+<template>
+    <button @click="handleClick">Click Me</button>
+</template>
